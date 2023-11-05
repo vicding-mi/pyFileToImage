1bf530a08f3cd1bd5bde891f1da9f2f6483c7ab9
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2023-11-05 14:24:38 +0100

    add dockerfile and create index page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import io
 import math
 from fastapi import FastAPI, Response, UploadFile, File
+from fastapi.responses import HTMLResponse, FileResponse
 from PIL import Image
 import numpy as np
 
@@ -9,7 +10,18 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return HTMLResponse("""
+    <html>
+        <head>
+            <title>FastAPI</title>
+        </head>
+        <body>
+            <h1>Encode and Decode JPG File</h1>
+            <p><a href="/image_encode">Encode</a></p>
+            <p><a href="/image_decode">Decode</a></p>
+        </body>
+    </html>
+    """)
 
 
 @app.get("/hello/{name}")
@@ -25,7 +37,21 @@ def find_nearest_factors(n):
     return factor1, factor2
 
 
-@app.post("/image_encode/")
+def get_upload_form(action: str, input_name: str = "image_file"):
+    return f"""
+    <form action="/{action}" enctype="multipart/form-data" method="post">
+    <input name="{input_name}" type="file" multiple>
+    <input type="submit">
+    </form>
+    """
+
+
+@app.get("/image_encode")
+async def upload_form():
+    return HTMLResponse(get_upload_form("image_encode"))
+
+
+@app.post("/image_encode")
 async def convert_binary_to_image(image_file: UploadFile = File(...)):
     data = await image_file.read()
 
@@ -36,7 +62,7 @@ async def convert_binary_to_image(image_file: UploadFile = File(...)):
     counter: int = 0
     for byte in data:
         counter += 1
-        print(f"{counter:04}: {byte} -> {bin(byte)[2:].zfill(m)}")
+        # print(f"{counter:04}: {byte} -> {bin(byte)[2:].zfill(m)}")
         arr[counter - 1, :] = [int(bit) for bit in bin(byte)[2:].zfill(m)]
 
     # converting from np array to image
@@ -48,7 +74,12 @@ async def convert_binary_to_image(image_file: UploadFile = File(...)):
 
     img = Image.fromarray(bw_arr.astype(np.uint8))
     img.save("encoded.jpg")
-    return Response(content=img.tobytes(), media_type="image/jpeg")
+    return FileResponse("encoded.jpg", filename="encoded.jpg", media_type="image/jpeg")
+
+
+@app.get("/image_decode")
+async def upload_form_decode():
+    return HTMLResponse(get_upload_form("image_decode"))
 
 
 @app.post("/image_decode")
@@ -57,9 +88,6 @@ async def convert_(image_file: UploadFile = File(...)):
 
     image_arr = np.array(image)
     bw_arr: np.ndarray = np.where(image_arr >= 100, 1, 0)
-    n, m = bw_arr.shape
-    n = n * m
-    m = 8
 
     bw_arr = bw_arr.flatten()
     binary_str: str = "".join(map(str, bw_arr))
